f1077eaba2edae585a8f02d2eb20f35cebe418d9
Martin J. Lenardon <martin.lenardon@L2Analytics.com>
2015-12-13 18:31:51 -0500

    Initial commit with ProjectScaffold

---
+++ src/TKV/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("TKV")>]
+[<assembly: AssemblyProductAttribute("TKV")>]
+[<assembly: AssemblyDescriptionAttribute("Time-Key-Value")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
