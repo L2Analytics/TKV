86184e44f965667bfac0f648b849c87101ae0abe
Martin J. Lenardon <martin.lenardon@L2Analytics.com>
2015-12-31 18:41:54 -0500

    Fixed errors in build, changed to using Value<'v> from Fact<'k, 'v>

--- src/TKV/Log.fs
+++ src/TKV/Log.fs
@@ -3,26 +3,31 @@
 module Log =
     let basic (storage: IStorage<'k, 'v>) (clock: IClock) :ILog<'k, 'v> =
         {new ILog<'k, 'v> with
-            member this.Append (f: Fact<'k, 'v>) = storage.Store (clock.Tick( )) f
+            member this.Append (k: 'k) (v: Value<'v>)= storage.Store (clock.Tick ()) k v
             member this.KeyTimes () = storage.List ()
             member this.TryFind (t: Time) (k: 'k) = 
                 let changeTime =
                     this.KeyTimes ()
                     |> List.filter (fun (_, k0) -> k0 = k)
                     |> List.map fst
-                    |> List.sortDescending
+                    // For some reason Visual Studio does not recognize List.sortDescending when building
+                    |> List.sort
+                    |> List.rev
                     |> List.tryFind (fun t0 -> t0 < t)
                 Option.bind (fun t0 -> storage.Retrieve t0 k) changeTime
-
+            
             member this.Snapshot (t: Time) :Snapshot<'k, 'v> =
                 this.KeyTimes ()
                 |> List.map (fun (t0, k0) -> (k0, t0))
-                |> List.groupBy (fun (k0, _) -> k0)
+                // For some reason Visual Studio does not recognize List.groupBy when building
+                |> Seq.ofList |> Seq.groupBy (fun (k0, _) -> k0) |> List.ofSeq |> List.map (fun (k, ktSeq) -> (k, List.ofSeq ktSeq))
                 |> Map.ofList
                 |> Map.map (fun _ kts -> List.map snd kts)
                 |> Map.map (fun _ ts -> 
                     ts
-                    |> List.sortDescending
+                    // For some reason Visual Studio does not recognize List.sortDescending when building
+                    |> List.sort
+                    |> List.rev
                     |> List.tryFind (fun t0 -> t0 < t)
                     )
                 |> Map.filter (fun _ t0 -> Option.isSome t0)
@@ -31,6 +36,10 @@ module Log =
                 |> List.map (fun (k, t) -> (k, storage.Retrieve t k))
                 |> List.filter (fun (_, v) -> Option.isSome v)
                 |> List.map (fun (k, v) -> (k, Option.get v))
+                |> List.choose (fun (k, value) ->
+                    match value with
+                    | Asserted v -> Some (k, v)
+                    | Retracted -> None)
                 |> Map.ofList
 
             member this.Timeseries (k: 'k) =
--- src/TKV/Types.fs
+++ src/TKV/Types.fs
@@ -3,12 +3,12 @@
 type Time = uint64
 type Date = System.DateTime
 
-type Fact<'k, 'v> =
-    | Assert of 'k*'v
-    | Retract of 'k
+type Value<'v> =
+    | Asserted of 'v
+    | Retracted
 
 type Snapshot<'k, 'v when 'k: comparison> = Map<'k, 'v>
-type Timeseries<'v> = Map<Time, 'v>
+type Timeseries<'v> = Map<Time, Value<'v>>
 
 type Agent<'msg> = MailboxProcessor<'msg>
 
@@ -19,14 +19,14 @@ type IClock =
     abstract member FindTime: Date -> Time
 
 type IStorage<'k, 'v> =
-    abstract member Store: Time -> Fact<'k, 'v> -> unit
-    abstract member Retrieve: Time -> 'k -> 'v option
+    abstract member Store: Time -> 'k -> Value<'v> -> unit
+    abstract member Retrieve: Time -> 'k -> Value<'v> option
     abstract member List: unit -> (Time*'k) list
 
 
 type ILog<'k, 'v when 'k: comparison> =
-    abstract member TryFind: Time -> 'k -> 'v option
-    abstract member Append: Fact<'k, 'v> -> unit
+    abstract member TryFind: Time -> 'k -> Value<'v> option
+    abstract member Append: 'k -> Value<'v> -> unit
     abstract member KeyTimes: unit -> (Time*'k) list
     abstract member Snapshot: Time -> Snapshot<'k, 'v>
     abstract member Timeseries: 'k -> Timeseries<'v>
